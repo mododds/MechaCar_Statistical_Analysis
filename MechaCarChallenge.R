a60d916adfa2af1e78226e331707354841ea03ae
Monica Dodds <93050600+mododds@users.noreply.github.com>
2022-03-13 13:27:52 -0400

    Uploading for Deliverable 3

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -14,3 +14,21 @@ Suspension_Coil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFa
 total_summary <- Suspension_Coil %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep') 
 #create summary table grouped by lots
 lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep') 
+
+# Deliverable 3
+# 1. 
+#generate 50 randomly sampled data points
+sample_table <- Suspension_Coil %>% sample_n(50) 
+# Run a t-test against a random sample
+t.test(log10(sample_table$PSI),mu=mean(log10(Suspension_Coil$PSI)))
+
+#2. 
+# Create subsets for each lot
+filter_lot1 <- subset(Suspension_Coil, Manufacturing_Lot == "Lot1") #filter for lot 1
+filter_lot2 <- subset(Suspension_Coil, Manufacturing_Lot == "Lot2") #filter for lot 2
+filter_lot3 <- subset(Suspension_Coil, Manufacturing_Lot == "Lot3") #filter for lot 3
+# Run a t-test against a each lot
+t.test(log10(filter_lot1$PSI),mu=mean(log10(Suspension_Coil$PSI)))
+t.test(log10(filter_lot2$PSI),mu=mean(log10(Suspension_Coil$PSI)))
+t.test(log10(filter_lot3$PSI),mu=mean(log10(Suspension_Coil$PSI)))
+
